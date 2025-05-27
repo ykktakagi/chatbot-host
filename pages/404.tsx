edbbce266e2a23da42b47e263773ae05b2ee54bd
ykktakagi <ykk.takagi@gmail.com>
2025-05-27 11:16:13 +0900

    Remove Tailwind config and add custom 404/500 pages

---
+++ pages/404.tsx
@@ -0,0 +1,4 @@
+// pages/404.tsx
+export default function Custom404() {
+  return <div style={{ padding: 20, textAlign: 'center' }}>ページが見つかりません。</div>;
+}
---
+++ pages/500.tsx
@@ -0,0 +1,4 @@
+// pages/500.tsx
+export default function Custom500() {
+  return <div style={{ padding: 20, textAlign: 'center' }}>サーバーエラーが発生しました。</div>;
+}
