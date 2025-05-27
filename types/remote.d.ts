b2d1a140aa8bb00799191ddc7a7e561e5725d97e
ykktakagi <ykk.takagi@gmail.com>
2025-05-27 09:12:32 +0900

    Add type declaration for makepdf_remort/MapWrapper

--- types/remote.d.ts
+++ types/remote.d.ts
@@ -1,4 +1,11 @@
+// types/remote.d.ts
+
 declare module 'makepdf_remort/ChartWrapper' {
+  const Component: React.ComponentType<any>;
+  export default Component;
+}
+
+declare module 'makepdf_remort/MapWrapper' {
   const Component: React.ComponentType<any>;
   export default Component;
 }
